4041ae2ab31bcfa531495ef8541c31e957d68486
luisservera <lservera03@gmail.com>
2024-05-11 11:52:18 +0200

    Fixed boolean argument passing to execution error

--- main.py
+++ main.py
@@ -7,17 +7,27 @@ import controller.controller as controller
 
 # TODO: improve/implement error handling
 # TODO: maybe send get user id request with a list (10 -> 100)
+# TODO: check if all the tweets are saved
 
 def main():
     check_excel = args.check_excel
+    print(check_excel)
     logging.info("Check excel set to: " + str(check_excel))
-    controller.execute(check_excel)
+    # controller.execute(check_excel)
+
+
+# Function to detect correctly boolean argument passed to execution
+def boolean_string(s):
+    if s == "True":
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
     # Needed to receive arguments when executing the script
     parser = argparse.ArgumentParser()
-    parser.add_argument("--check_excel", required=True, type=bool)
+    parser.add_argument("--check_excel", required=True, type=boolean_string)
     args = parser.parse_args()
 
     # Needed to keep track of the execution logs
